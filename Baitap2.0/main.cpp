cbe067142152eeb60c2e17b30dccf8310b54edfc
Tienthanh44 <thanhpanda954@gmail.com>
2021-05-13 22:48:00 +0700

    first commit

--- Baitap2.0/main.cpp
+++ Baitap2.0/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
 using namespace std;
-
+void songuyento (int x)
+{
+    if(x<2){
+        return false;
+    }
+    else {
+        for(int i=2;i<x;i++){
+            if(x%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
 int main()
 {
     cout<<"Nhap n:";
@@ -35,45 +48,11 @@ int main()
             cout<<" "<<a[i];
         }
     }
-    for(int i=0; i<n; i++)
-    {
+    cout<<"\n Cac so nguyen to co trong mang la :";
+    for (int i=0;i<n;i++){
+        if(songuyento(a[i])){
+            cout<<a[i]<<" ";
+        }
+    }
 
-        if(a[i]<2)
-        {
-            return false;
-        }
-        else
-        {
-            if(a[i]==2)
-            {
-                return true;
-            }
-            else {
-            if(a[i]%2==0)
-                {
-                    return false;
-                }
-
-                else
-            {
-                for (int i=2; i<a[i]-1; i++)
-                    {
-                        if(a[i]%i==0)
-                        {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-            return true;
-        }
-        cout<<"Day so nguyen to la :";
-        for (int i=0; i<n; i++)
-        {
-            if (a[i]=true)
-            {
-                cout<<" "<<a[i];
-            }
-        }
     }
